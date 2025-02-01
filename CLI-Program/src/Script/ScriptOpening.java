26cbfad82521d70b2a7b7d264d6fd1c4147b2bd6
chulsu0012 <chulsu0012@gmail.com>
2025-02-02 01:50:25 +0900

    #3: 클래스 계층 구조 변경, Taxi 클래스 추가

--- CLI-Program/src/Script/ScriptOpening.java
+++ CLI-Program/src/Script/ScriptOpening.java
@@ -1,5 +1,7 @@
-public class ScriptOpening extends Script{
-    String[] opening = new String[] {
+package Script;
+
+public class ScriptOpening extends Script {
+    private final String[] opening = new String[] {
             line,
             letter,
             "                판교역에서",
@@ -13,11 +15,11 @@ public class ScriptOpening extends Script{
             "\t  출근 시간대와 겹쳐 거리가 너무나 혼잡합니다.\n",
             "\t✦ 무사히 유스케이스 건물에 도착할 수 있을까요?\n",
             line,
-            "\t✦ 버스는 미어터지고 길거리는 춥습니다.",
-            "\t  걸어갈까요, 버스를 탈까요?\n",
-            "\t  1. 걸어간다.",
-            "\t  2. 버스를 탄다.",
     };
+
+    public void typingOpening() {
+        typing(opening);
+    }
 
 
 }
